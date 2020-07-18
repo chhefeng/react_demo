eae6b8c9372156d729c0d2872107842b54d7dc83
Hefeng <chhefeng@gmail.com>
2020-07-18 14:38:06 +0200

    add header, products

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Header extends React.Component {
+
+    render(){
+        return(
+            <div className="header">
+                <p>Header Component</p>
+            </div>
+        );
+    }
+}
+
+export default Header;
